915cdfd23fdbb0725c5d1eee9de64634198d1fbd
CHIU-YIKAI <andy757588596@gmail.com>
2023-01-09 20:22:19 +0800

    Update TrelloBoard

--- app/trello-board/trello-board.component.ts
+++ app/trello-board/trello-board.component.ts
@@ -13,7 +13,7 @@ export class TrelloBoardComponent implements OnInit {
   readonly list2 = [{card_id:"1",card_title : "PEP撰寫", card_content:"test1516515616" },{card_id:"2",card_title : "SRS撰寫", card_content:"test" },{card_id:"3",card_title : "SDD撰寫", card_content:"test" }];
   readonly list3 = [{card_id:"1",card_title : "PEP撰寫", card_content:"test1516515616" },{card_id:"2",card_title : "SRS撰寫", card_content:"test" },{card_id:"3",card_title : "SDD撰寫", card_content:"test" }];
   readonly list4 = [{card_id:"1",card_title : "PEP撰寫", card_content:"test1516515616" },{card_id:"2",card_title : "SRS撰寫", card_content:"test" },{card_id:"3",card_title : "SDD撰寫", card_content:"test" }];
-  readonly board = [{list_name: "文件撰寫", list_data: this.list1},{list_name:"程式撰寫",list_data:this.list2},{list_name:"進度報告",list_data:this.list3},{list_name:"問題討論",list_data:this.list4}];
+  readonly board = [{list_name: "文件撰寫", list_data: this.list1, list_ID: "jasoijfokwqemfoijnvoi"},{list_name:"程式撰寫",list_data:this.list2, list_ID: "uvpoizxvaslkmfw"},{list_name:"進度報告",list_data:this.list3, list_ID:"xcmvwpoijefvzdajqs"},{list_name:"問題討論",list_data:this.list4, list_ID: "1x6c1v6s41f61d68f4s"}];
   board_name
   test = ["123","456","789"]
   name = "jason"
@@ -23,6 +23,8 @@ export class TrelloBoardComponent implements OnInit {
   ngOnInit(): void {
     window.scrollTo(0, 0);
     this.getListTrack();
+
+
   }
 
   getListTrack() {
@@ -31,9 +33,16 @@ export class TrelloBoardComponent implements OnInit {
     //TODO
   }
 
-  getIssueTrack() {
+
+  creatNewCard(value:any){
+    console.log(value)
+    var name = prompt("卡片名稱");
+    var Data = prompt("卡片內容");
 
   }
 
-
+  deleteCard(boardID: any, cardID :any){
+    console.log(boardID)
+    console.log(cardID)
+  }
 }
